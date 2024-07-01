f788af397d5352cac8b67d7b9b66a32d3488c847
SrijanRoy14 <125462792+SrijanRoy14@users.noreply.github.com>
2024-07-02 00:31:45 +0530

    initial commit with limited features

---
+++ app.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import preprocessor as pre
+import helper
+import matplotlib.pyplot as plt
+st.sidebar.title("WhatsApp Chat Analyser")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    data= bytes_data.decode("utf-8")
+    df=pre.preprocess(data)
+    
+    st.dataframe(df)
+
+    #fetch unique users
+    user_list=df['user'].unique().tolist()
+    user_list.remove('group_notification')
+    user_list.sort()
+    user_list.insert(0,"Overall")
+
+    selected_user=st.sidebar.selectbox("Show analysis wrt",user_list)
+    num_messages,words,num_media_msg,num_links=helper.fetch_stats(selected_user,df)
+
+    if st.sidebar.button("Show Analysis"):
+        
+        col1,col2,col3,col4=st.columns(4)
+        with col1:
+            st.header("Total Messages")
+            st.title(num_messages)
+        with col2:
+            st.header("Total words")
+            st.title(words)
+        with col3:
+            st.header("Total media shared")
+            st.title(num_media_msg)
+        with col4:
+            st.header("Links shared")
+            st.title(num_links)
+        
+        #find the busiest user in the group
+        if selected_user=='Overall':
+            st.title('Most active user')
+            x,new_df=helper.fetch_most_busy_user(df)
+            fig,ax=plt.subplots()
+           
+            col1,col2=st.columns(2)
+
+            with col1:
+                ax.bar(x.index,x.values,color='red')
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+            with col2:
+                st.dataframe(new_df)
+        #wordcloud
+        df_wc=helper.create_wordcloud(selected_user,df)
+        fig,ax=plt.subplots()
+        ax.imshow(df_wc)
+        st.pyplot(fig)
+
+        #most_common_words
---
+++ helper.py
@@ -0,0 +1,43 @@
+from urlextract import URLExtract
+from wordcloud import WordCloud
+extract=URLExtract()
+def fetch_stats(selected_user,df):
+    if selected_user!='Overall':
+        df=df[df['user']==selected_user]
+
+        #1. number of messages
+    num_messages=df.shape[0]
+        #2. number of words
+    words=[]
+    for message in df['message']:
+        words.extend(message.split())
+    #3. fetch no. of media messages
+    num_media_msg=df[df['message']=='<Media omitted>\n'].shape[0]
+
+    #4fetch number of links
+    links=[]
+    for message in df['message']:
+        links.extend(extract.find_urls(message))
+
+    return num_messages,len(words),num_media_msg,len(links)
+    
+def fetch_most_busy_user(df):
+    x=df['user'].value_counts().head()
+    df=round((df['user'].value_counts()/df.shape[0])*100,2).reset_index().rename(
+    columns={'index':'name','user':'percent'}
+    )
+    return x,df
+def create_wordcloud(selected_user,df):
+    if selected_user!='Overall':
+        df=df[df['user']==selected_user]
+    wc=WordCloud(width=500,height=500,min_font_size=10,background_color='white')
+    df_wc=wc.generate(df['message'].str.cat(sep=" "))
+    return df_wc
+
+def most_commwords(selected_user,df):
+    if selected_user!='Overall':
+        df=df[df['user']==selected_user]
+    temp=df[df['user']!='group_notification']
+    temp=temp[temp['message']!='<Media omitted>\n']
+    words=[]
+    
